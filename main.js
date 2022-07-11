d5b1771146947d8a0b298e2f0b723491cac3223a
Bili <jambon_bilitis@hotmail.com>
2022-07-11 16:31:45 +0200

    add fichier main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 const urlApi = 'http://localhost:8888/forum/forum-api_php/ticket';
+const ticketMessage = document.querySelector('.publish_ticket_message');
+const ticketUser  = document.querySelector('.publish_ticket_user');
+
+//listing des tickets
+fetch(urlApi + 'categories')
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        let template = ''
+        response.data.forEach(category => {
+            template += `<li class="cat-item" data-id="${category.id_categorie}">${category.nom}</li>`
+        });
+        catListe.innerHTML = template
+    })
+    .catch(error => console.error(error));
